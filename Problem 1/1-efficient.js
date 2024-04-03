17bb751b3952bbdbf9ca268ccb09534a0b159825
yasin1ar <yasin.amirany@gmail.com>
2024-04-03 23:52:11 +0330

    problem 4 added

--- Problem 1/1-efficient.js
+++ Problem 1/1-efficient.js
@@ -1,3 +1,5 @@
+// Problem 1 : https://projecteuler.net/problem=1
+
 /*
 
 psudocode for efficient Solution:
@@ -13,14 +15,14 @@ Solution caught from https://projecteuler.net/overview=0001
 
 */
 
-function efficientSolution(target){
-	function sumDivisibleBy(n){
-		const p = Math.floor(target / n);
-		return n * (p*(p+1)) / 2;
-	}
+const efficientSolution = (target) => {
+  const sumDivisibleBy = (n) => {
+    const p = Math.floor(target / n);
+    return (n * p * (p + 1)) / 2;
+  };
 
-	let sum = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15);
-	console.log(sum);
-}
+  const sum = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15);
+  console.log(sum);
+};
 
 efficientSolution(1000);
--- Problem 1/1-simple.js
+++ Problem 1/1-simple.js
@@ -8,25 +8,22 @@
 
 // Function to check if a number is a multiple of 3 or 5
 function isMultiplies(number) {
-    // Check if the number is divisible by 3 or 5 using the modulo operator (%)
-    if (number % 3 === 0 || number % 5 === 0) {
-      return true; // Return true if the number is a multiple
-    }
+  // Check if the number is divisible by 3 or 5 using the modulo operator (%)
+  if (number % 3 === 0 || number % 5 === 0) {
+    return true; // Return true if the number is a multiple
   }
-  
+}
+
 // Initialize variables
 let sum = 0;
 const target = 1000;
 
 // Loop through numbers from 1 to target-1
 for (let i = 1; i < target; i++) {
-    // Check if the number is a multiple of 3 or 5
-    if (isMultiplies(i)) {
-        sum += i; // Add the number to the sum
-    }
+  // Check if the number is a multiple of 3 or 5
+  if (isMultiplies(i)) {
+    sum += i; // Add the number to the sum
+  }
 }
 
 console.log(sum); // Output the sum of the multiples
-
-
-
