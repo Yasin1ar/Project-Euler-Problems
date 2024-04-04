f4ff15707933e3aa155f86fa1221ba8854626c1d
yasin1ar <yasin.amirany@gmail.com>
2024-04-04 19:07:32 +0330

    added 4-simple

---
+++ Problem 4/4-simple.js
@@ -0,0 +1,47 @@
+// Problem 4 : https://projecteuler.net/problem=4
+
+/*
+    pseudocode :
+        function isPalindrome(number):
+            reversedNum 
+            n = number;
+            while (n >= 1):
+                digit = n % 10;
+                reversedNum = (reversedNum * 10) + digit;
+                n = round(n / 10);
+            return number == reversedNum
+
+        largestPalindromeNum = 0;
+        for i in range 999 -> 100:
+            for j in range 999 -> 100:
+                if isPalindrome((i*j)) AND (i*j) > largestPalindrome:
+                    largestPalindromeNum = (i*j)
+        
+        print largestPalindromeNum
+
+*/
+
+function isPalindrome(number) {
+  let reversedNum = 0;
+  let n = number;
+  while (n >= 1) {
+    let digit = n % 10;
+    reversedNum = reversedNum * 10 + digit;
+    n = ~~(n / 10);
+  }
+  return number === reversedNum;
+}
+
+let largestPalindrome = 0;
+for (let i = 999; i > 100; i--) {
+  for (let j = 999; j > 100; j--) {
+    let number = i * j;
+    if (isPalindrome(number)) {
+      if (number > largestPalindrome) {
+        largestPalindrome = number;
+      }
+    }
+  }
+}
+
+console.log(largestPalindrome);
